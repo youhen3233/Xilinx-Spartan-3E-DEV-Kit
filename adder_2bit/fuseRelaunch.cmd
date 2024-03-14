04272589d99e599e25db81e6280649e4c56325e2
youhen3233 <42541363+youhen3233@users.noreply.github.com>
2024-03-14 13:45:02 +0800

    Delete adder_2bit directory

--- adder_2bit/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Youhen/adder_2bit/tb_adder_2bit_isim_beh.exe" -prj "C:/Youhen/adder_2bit/tb_adder_2bit_beh.prj" "work.tb_adder_2bit" "work.glbl" 
